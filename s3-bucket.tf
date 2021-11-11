2a49c0d61d82c6c6b9b625a5585d8c308154dd34
David Ball <david@balld31.com>
2021-11-11 21:01:31 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,13 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "Gaurav"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  bucket_prefix = "davidball"
+
+}
