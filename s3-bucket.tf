cbcc96f7525c999f359c33a82e2fc853dc484980
David Ball <david@balld31.com>
2021-11-11 21:04:43 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source = "app.terraform.io/tf-chip-training-dball/s3-bucket/aws"
+  version = "2.2.0"
 
   bucket = "Gaurav"
   acl    = "private"
